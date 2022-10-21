942aa1247c5164b57f110709dce32945141498f0
timmac62 <25373189+timmac62@users.noreply.github.com>
2022-10-21 01:26:30 -0400

    Use exists() to prevent unnecessary Homebrew installs

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,7 +1,15 @@
 #!/usr/bin/env zsh
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+if exists brew; then
+  echo "brew exists, skipping install"
+else
+  echo "brew does not exist, continueing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+
 
 # TODO: keep an eye out for a different '--no-quarantine' solution.
 # Currently you cannot do a 'brew bundle --no-quarantine' as an option.
