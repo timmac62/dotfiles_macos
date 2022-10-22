179943604403bd3fae519976286a0f6e5fdd0c56
timmac62 <25373189+timmac62@users.noreply.github.com>
2022-10-21 23:57:41 -0400

    Prevent superfluous commands with conditionals.

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -2,9 +2,31 @@
 
 echo "\n<<< Starting ZSH Setup >>>\n"
 
-# installation unnecessary; it's in the Brewfile.
+# https://stackoverflow.com/a/4749368/1341838
+if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells' ; then
+  echo "/opt/homebrew/bin/zsh already exists in /etc/shells"
+else
+  echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
 
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+# echo "Enter superuser (sudo) password to edit /etc/shells"
+# echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 
-chsh -s '/opt/homebrew/bin/zsh'
+if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
+  echo '$SHELL is already /opt/homebrew/bin/zsh'
+else
+  echo "Enter user password to change login shell"
+  chsh -s '/opt/homebrew/bin/zsh'
+fi
+
+if sh --version | grep -q zsh; then
+  echo '/private/var/select/sh already linked to /opt/homebrew/bin/zsh'
+else
+  echo "Enter superuser (sudo) password to symlink sh to zsh"
+  # looked cute, might delete later, idk
+  sudo ln -sfv /bin/zsh /private/var/select/sh
+
+  # I'd like for this to work instead
+  # sudo ln -sfv /opt/homebrew/bin/zsh /private/var/select/sh
+fi
--- shebang.zsh
+++ shebang.zsh
@@ -1,9 +1,6 @@
-#!/usr/bin/env zsh
 
 echo "\n<<< Running $0 >>>\n"
 echo "ZSH_VERSION = ${ZSH_VERSION}"
+echo "The current shell is: $(ps $$ -ocomm=)"
 
-exists brew && echo "excellent!!!" || echo "bogus"
-exists nonexistent && echo "excellent!!!" || echo "bogus"
-
-which exists
+sleep 10; echo done;
