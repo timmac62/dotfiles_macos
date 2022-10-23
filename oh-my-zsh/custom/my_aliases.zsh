dadb068d149242bc4811259fc9e3f1d5040b8024
timmac62 <25373189+timmac62@users.noreply.github.com>
2022-10-23 15:31:37 -0400

    New aliases

--- oh-my-zsh/custom/my_aliases.zsh
+++ oh-my-zsh/custom/my_aliases.zsh
@@ -1,6 +1,23 @@
+# <<<<<<<<<< Simple Aliases
+
 alias ls='exa -laFh --git'
 alias exa='exa -laFh --git'
 alias trail='<<<${(F)path}'
 alias man=batman
 alias bbd='brew bundle dump --force --describe'
+alias sc="source $HOME/.zshrc"
 # alias rm=trash
+
+# <<<<<<<<<< Suffix Aliases
+
+# Markdown files
+alias -s md=code
+# JSON files
+alias -s json=code
+# bulk association
+alias -s {cs,ts,html}=code
+
+# <<<<<<<<<< Global Aliases
+
+# alias -g aliasName='command'
+
