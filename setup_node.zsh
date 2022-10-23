f8e3511e1a7ac037b85a5eecda6fc50822e2bce5
timmac62 <25373189+timmac62@users.noreply.github.com>
2022-10-23 05:14:58 -0400

    Removed installs I do not use.

--- setup_node.zsh
+++ setup_node.zsh
@@ -13,11 +13,7 @@ else
 fi
 
 # Install Global NPM Packages
-npm install --global firebase-tools
-npm install --global @angular/cli
-npm install --global @ionic/cli
 npm install --global typescript
-npm install --global json-server
 npm install --global http-server
 npm install --global trash-cli
 
